40d48718cde8a1b571a272094213db725e2c896a
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2018-11-14 18:20:13 -0500

    Functional Bot

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import discord
+import KeepAlive
+import BunnyBuilder
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        if message.content.startswith("!bunnyhelp"):
+            await message.channel.send(sendHelp())
+        elif message.content.startswith("!bunny"):
+            await message.channel.send(generateBunny(message = message.content[6::]))   
+
+def generateBunny(message: str):
+    bunny = f"```{BunnyBuilder.buildSign(message)}```"
+    return bunny
+
+def sendHelp():
+    message = "Write a message after `!bunny` to put it in a sign "
+    return message
+
+KeepAlive.keepAlive()
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
+sys.stdout.flush()
