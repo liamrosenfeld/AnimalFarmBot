0ea013817a6b58a7db87d347297ba313b2d80850
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2019-02-04 18:53:51 -0500

    Stats During Debug

--- Sources/AnimalFarm/Setup/main.swift
+++ Sources/AnimalFarm/Setup/main.swift
@@ -24,6 +24,19 @@ let bot = Shield(token: token, swordOptions: swordOptions, shieldOptions: shield
 
 #if DEBUG
     bot.editStatus(to: "online", playing: "Testing (So Be Nice) :)")
+
+    bot.register("stats") { msg, _ in
+        print("Servers:")
+        for guild in bot.guilds {
+            print(guild.value.name)
+        }
+        
+        var message = "I Am In...\n"
+        message.append("\(bot.guilds.count) Servers\n")
+        message.append("\(bot.dms.count) DMs\n")
+        message.append("\(bot.groups.count) Groups\n")
+        msg.reply(with: message)
+    }
 #endif
 
 bot.addUtilities()
