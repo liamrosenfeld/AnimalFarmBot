217bf6e639618485bbed63e58710ef57f73fa21c
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2019-02-04 19:48:55 -0500

    Production Stats

--- Sources/AnimalFarm/Setup/Shield+AddCommands.swift
+++ Sources/AnimalFarm/Setup/Shield+AddCommands.swift
@@ -15,12 +15,12 @@ extension Shield {
             let animal = animalMaker.build(with: ["Pong!"])
             msg.reply(with: animal)
         }
-        
+
         self.register("info", message: """
                             <> with ❤ By Liam Rosenfeld.
                             Check Out The Source Code Here: https://github.com/liamrosenfeld/AnimalFarmBot
                             """)
-        
+
         self.register("help", message: """
                             `!<animal> <message>` -> animal says the message
                             `!animal` -> Lists supported animals
@@ -28,13 +28,11 @@ extension Shield {
                             `!help` -> Bring up this help menu
                             `!info` -> Show Credits and Link to Source Code
                             """)
-        
-        
     }
-    
+
     func addAnimals() {
         var helpText = "Supported Animals:"
-        
+
         Animal.allCases.forEach { animal in
             let name = String(describing: animal)
             self.register(name) { msg, args in
@@ -44,15 +42,46 @@ extension Shield {
             }
             helpText += "\n- `\(name)`"
         }
-        
+
         self.register("random") { msg, args in
             let animalMaker = AnimalBuilder(animal: Animal.allCases.randomElement() ?? .bunny)
             let animal = animalMaker.build(with: args)
             msg.reply(with: animal)
             helpText += "\n- `random`"
         }
-        
+
         self.register("animals", message: helpText)
+    }
+
+    func addPublicStats() {
+        self.register("stats") { _, _ in
+            print("\nServers:")
+            for guild in self.guilds {
+                print(" - \(guild.value.name)")
+            }
+
+            print("\n")
+
+            print("Totals:")
+            print("\(self.guilds.count) Servers")
+            print("\(self.dms.count) DMs")
+            print("\(self.groups.count) Groups")
+        }
+    }
+
+    func addPrivateStats() {
+        self.register("stats") { msg, _ in
+            print("\nServers:")
+            for guild in self.guilds {
+                print(" - \(guild.value.name)")
+            }
+
+            var message = "I Am In:\n"
+            message.append("\(self.guilds.count) Servers\n")
+            message.append("\(self.dms.count) DMs\n")
+            message.append("\(self.groups.count) Groups\n")
+            msg.reply(with: message)
+        }
     }
 }
 
